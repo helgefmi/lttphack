5f0acf1c5b8b4ffa7e2fcea83cbc78601b22be5a
Helge Milde <helge@monsternett.no>
2017-11-05 15:58:45 +0100

    Add in the required transparant tile for custom menu tileset

--- src/custom_menu.asm
+++ src/custom_menu.asm
@@ -391,7 +391,7 @@ cm_transfer_tileset:
     LDX #$7000 : STX $2116 ; VRAM address (E000 in vram)
     LDX #cm_hud_table : STX $4302 ; Source offset 
     LDA #$25 : STA $4304 ; Source bank
-    LDX #$0A00 : STX $4305 ; Size (0x10 = 1 tile)
+    LDX #$0900 : STX $4305 ; Size (0x10 = 1 tile)
     LDA #$01 : STA $4300 ; word, normal increment (DMA MODE)
     LDA #$18 : STA $4301 ; destination (VRAM write)
     LDA #$01 : STA $420B ; initiate DMA (channel 1)
