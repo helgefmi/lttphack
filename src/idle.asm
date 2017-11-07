31fe1a3a33aa52152e44a8c649d200f4313f121a
Helge Milde <helge@monsternett.no>
2017-11-07 20:13:51 +0100

    Count idle frames in menu too

--- src/idle.asm
+++ src/idle.asm
@@ -1,3 +1,4 @@
+; Waitkey
 org $0EFB90
     ; 0efb90 lda $00f4
     ; 0efb93 ora $00f6
@@ -7,18 +8,29 @@ org $0EFB90
     NOP : NOP
 
 
+; EndMessage
 org $0efbbb
     ; 0efbbb lda $f4
     ; 0efbbd ora $f6
     JSL idle_endmessage
 
+; MenuActive
+org $0DDF1E
+    ; 0ddf1e lda $f4
+    ; 0ddf20 and #$10
+    JSL idle_menu
+
+
+; BottleMenu
+org $0DE0E2
+    ; 0de0e2 lda $f4
+    ; 0de0e4 and #$10
+    JSL idle_menu
 
 org $28B000
+
 idle_waitkey:
-    LDA $00F4
-    ORA $00F6
-    AND #$C0
-    BNE .pressed_key
+    LDA $00F4 : ORA $00F6 : AND #$C0 : BNE .pressed_key
 
   %a16()
     INC !lowram_idle_frames
@@ -29,9 +41,7 @@ idle_waitkey:
     RTL
 
 idle_endmessage:
-    LDA $00F4
-    ORA $00F6
-    BNE .pressed_key
+    LDA $00F4 : ORA $00F6 : BNE .pressed_key
 
   %a16()
     INC !lowram_idle_frames
@@ -39,4 +49,16 @@ idle_endmessage:
 
   .pressed_key
     ORA $00F6
+    RTL
+
+
+idle_menu:
+    LDA $F4 : ORA $F6 : BNE .pressed_key
+
+  %a16()
+    INC !lowram_idle_frames
+  %a8()
+
+  .pressed_key
+    AND #$10
     RTL
