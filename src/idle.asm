224bd400b84385e18bee3df9153b1137111802f6
spannerisms <32842036+spannerisms@users.noreply.github.com>
2019-10-15 07:10:37 -0400

    clean up idle frames a little
    non disruptive cycle saves by removing extraneous stuff
    addr->dp because vanilla devs are weird
    ORA const instead of dp esp since it might technically be possible to lag so much to get a different value never

--- src/idle.asm
+++ src/idle.asm
@@ -5,8 +5,8 @@ org $0EFB90
 	; 0efb93 ora $00f6
 	; 0efb96 and #$c0
 	; 0efb98 beq $fba6
-	JSL idle_waitkey
-	NOP #2
+	LDA $F4 ; vanilla pointless used address; dp is better
+	JSL idle_waitkey ; now we have an easy 4 bytes here for the JSL
 
 
 ; EndMessage
@@ -32,26 +32,26 @@ org $0DE0E2
 pullpc
 
 idle_waitkey:
-	LDA $00F4 : ORA $00F6 : AND #$C0 : BNE .pressed_key
+	; LDA $F4 from entry point
+	ORA $F6 : AND #$C0 : BNE .pressed_key 
 
 	%a16()
 	INC !lowram_idle_frames
 	%a8()
 
 .pressed_key
-	AND #$C0
 	RTL
 
 
 idle_endmessage:
-	LDA $00F4 : ORA $00F6 : BNE .pressed_key
+	LDA $F4 : ORA $F6 : BNE .pressed_key
 
 	%a16()
 	INC !lowram_idle_frames
 	%a8()
 
 .pressed_key
-	ORA $00F6
+	ORA #$FF ; get A to trigger P flags
 	RTL
 
 
