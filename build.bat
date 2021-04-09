8ddbfc41920395ce10d831151baec51ddc162975
spannerisms <32842036+spannerisms@users.noreply.github.com>
2021-04-09 01:34:56 -0400

    patches

--- build.bat
+++ build.bat
@@ -5,9 +5,18 @@ copy alttp.sfc "lttphack-%version% SD2SNES.sfc"
 copy alttp.sfc "lttphack-%version% VanillaHUD.sfc"
 copy alttp.sfc "lttphack-%version% SD2SNES VanillaHUD.sfc"
 cd ../src
+
 asar.exe -DFEATURE_SD2SNES=0 -DFEATURE_HUD=1 -DVERSION=%version% main.asm "../target/lttphack-%version%.sfc"
 asar.exe -DFEATURE_SD2SNES=1 -DFEATURE_HUD=1 -DVERSION=%version% main.asm "../target/lttphack-%version% SD2SNES.sfc"
 asar.exe -DFEATURE_SD2SNES=0 -DFEATURE_HUD=0 -DVERSION=%version% main.asm "../target/lttphack-%version% VanillaHUD.sfc"
 asar.exe -DFEATURE_SD2SNES=1 -DFEATURE_HUD=0 -DVERSION=%version% main.asm "../target/lttphack-%version% SD2SNES VanillaHUD.sfc"
+
+cd ../target
+
+flips --create --bps alttp.sfc "lttphack-%version%.sfc" "../docs/patcher/files/emulator-customhud.bps"
+flips --create --bps alttp.sfc "lttphack-%version% VanillaHUD.sfc" "../docs/patcher/files/emulator-vanillahud.bps"
+flips --create --bps alttp.sfc "lttphack-%version% SD2SNES.sfc" "../docs/patcher/files/sd2snes-customhud.bps"
+flips --create --bps alttp.sfc "lttphack-%version% SD2SNES VanillaHUD.sfc" "../docs/patcher/files/sd2snes-vanillahud.bps"
+
 cd ..
 pause
